e4de18f016e32167e3d8f7e47e500711bd723834
Judy Hanson <judy.l.hanson@gmail.com>
2023-09-26 21:18:55 -0700

    added server and data.js for json data

---
+++ server/data.js
@@ -0,0 +1,15 @@
+
+const plans = {
+    'bronze': {
+      'deductable': 10000,
+      'outOfPocketMax': 20000, 
+      'rx': 500
+    }, 
+    'silver': {
+        'deductable': 5000,
+        'outOfPocketMax': 10000, 
+        'rx': 200
+      }
+}
+
+module.exports = plans
