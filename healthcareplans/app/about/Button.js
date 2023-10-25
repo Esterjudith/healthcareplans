0f5efe764d58d17e424b17ac4ce1625452105348
Judy Hanson <judy.l.hanson@gmail.com>
2023-10-25 10:47:40 -0700

    updated the connection to Supabase, added .env.local file

---
+++ healthcareplans/app/about/Button.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+
+const Button = () => {
+    return (
+        <button>
+           Hello 
+        </button>
+    );
+};
+
+export default Button;
---
+++ healthcareplans/app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './button'
+
+const Aboutpage = () => {
+  return (
+    <div>        
+        <h1>About page </h1> 
+        <Button />      
+    </div>
+  )
+}
+
+export default Aboutpage
