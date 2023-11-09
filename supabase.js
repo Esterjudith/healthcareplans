63775f327cb08ea3e6f6ef92dbae3c6c0669e41f
Judy Hanson <judy.l.hanson@gmail.com>
2023-11-09 14:00:15 -0700

    reinstalled nextjs, installed supabase

---
+++ supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseURL, supabaseAnonKey)
+//This code imports the createClient, creates a Supabase client instance using the SupabaseURL and anonymous key from env variables, and export the client instance. 
